b146c4d27d2e86ebbf24da4572a41af56ccd4b5d
Remy-cloud <r.intwali@alustudent.com>
2026-05-23 16:10:19 +0200

    coloblindness mode

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,29 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
+    private Color trapOriginalColor = Color.red;
+    private Color goalOriginalColor = Color.green;
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 255);
+            goalMat.color = Color.blue;
+        }
+        else
+        {
+            trapMat.color = trapOriginalColor;
+            goalMat.color = goalOriginalColor;
+        }
+
         SceneManager.LoadScene("Maze");
     }
 
