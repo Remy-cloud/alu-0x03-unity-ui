5a012f6c7f3a97bc71ed759943664d5534093fc2
Remy-cloud <r.intwali@alustudent.com>
2026-05-23 15:10:45 +0200

    quit game button functionality

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,4 +7,10 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("Maze");
     }
+
+    public void QuitMaze()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
 }
